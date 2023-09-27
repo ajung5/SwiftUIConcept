1fe839d5cad46c745d428186b2a9e92636528e11
Agung Nawawi <agung@fairtech.com.sg>
2023-09-27 07:40:16 +0700

    Initial Commit

---
+++ SwiftUIConcept/SwiftUIConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConceptApp.swift
+//  SwiftUIConcept
+//
+//  Created by Agung Nawawi on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
